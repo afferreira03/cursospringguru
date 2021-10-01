6b6e5304a4a84106b9195e7870c34548d587f4ba
Alan Felipe Ferreira <alan.fferreira03@gmail.com>
2021-10-01 17:33:16 -0300

    chore initial commit

---
+++ src/main/java/com/singular/cursospringguru/CursospringguruApplication.java
@@ -0,0 +1,13 @@
+package com.singular.cursospringguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringguruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringguruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singular/cursospringguru/CursospringguruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singular.cursospringguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringguruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
